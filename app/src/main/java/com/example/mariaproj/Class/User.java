28d9c0217a9a75b508837630e65beb14f35ca947
MariaNoufi <marianoufi.296@gmail.com>
2023-09-06 14:23:54 +0300

    Fist step

---
+++ app/src/main/java/com/example/mariaproj/Class/User.java
@@ -0,0 +1,10 @@
+package com.example.mariaproj.Class;
+
+public class User {
+    private String email;
+    private String password;
+    public(String email , String password){
+        this.email=email;
+        this.password=password;
+    }
+}
